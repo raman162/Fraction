bb6bf381ee7064799953aad8007af6a025119227
Raman Walwyn-Venugopal <ramanvenu94@gmail.com>
2016-01-26 17:25:17 -0600

    Added the main method to the fraction.rb and finished all of the functions

--- fraction.rb
+++ fraction.rb
@@ -12,9 +12,46 @@ class Fraction
     @num=n1+n2
     @den=l
   end
-  
-  
 
+  def radd(fraction)
+    num=fraction.getnum()
+    den=fraction.getden()
+    l=@den.lcm(den)
+    n1=@num*(l/@den)
+    n2=num*(l/den)
+    n3=n1+n2
+    return Fraction.new(n3, l)
+
+  end
+
+  def getnum()
+    return @num
+  end
+
+  def getden()
+    return @den
+  end
+  
+  def self.multiplication(fraction1,fraction2)
+    num1=fraction1.getnum()
+    num2=fraction2.getnum()
+    den1=fraction1.getden() 
+    den2=fraction2.getden()
+    num3=num1*num2
+    den3=den1*den2
+    fraction3=Fraction.new(num3,den3)
+    return fraction3
+  end
+
+  def print()
+    puts "#{@num}/#{@den}"
+  end
 
 end
 
+half=Fraction.new(1,2)
+threeq=Fraction.new(3,4)
+addfrac=half.radd(threeq)
+multfrac=Fraction.multiplication(half,threeq)
+addfrac.print()
+multfrac.print()
